a9d50e9b28a8494f1bb302db6fcc9c78ffc00fa6
Joe Nash <joenash@blackvine.co.uk>
2014-03-22 19:05:33 +0000

    Update ask_questions.rb -#CleanUpGithub
    This comment has been identified as either hateful, hurtful or discriminatory as part of a project to #CleanUpGitHub. This pull request was made by a human. For more info, please check EthicalCode’s website at www.ethicalco.de/cleanupgh

--- learn_to_program/ask_questions.rb
+++ learn_to_program/ask_questions.rb
@@ -4,11 +4,11 @@ def ask question
     reply = gets.chomp.downcase
     return true if reply == 'yes'
     return false if reply == 'no'
-    puts 'Nigga please answer yes or no'
+    puts 'Please answer yes or no'
   end
 end
 
-ask 'do you suck cocks'
+ask 'do you trumpets'
 ask 'do you blow into them'
 does_pile_have_normal_sibilings = ask 'do your parents have normal children as well, private Pile?'
 puts does_pile_have_normal_sibilings
